b6efa2c47aed205a77e36ca2905a7cdd7221a693
youmerite <7wzkrqvvbw@privaterelay.appleid.com>
2026-01-15 00:58:03 +0100

    Add stest.js with sample data structure

---
+++ api/stest.js
@@ -0,0 +1,9 @@
+{
+  "name": "Test",
+  "phone": "123",
+  "city": "TestŞehir",
+  "delivery_address": "Adres",
+  "delivery_type": "Normal",
+  "cart_items": ["Ürün1", "Ürün2"],
+  "total_price": 100
+}
