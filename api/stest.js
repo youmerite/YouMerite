3308133cc4f1bddb1f360f473066439ca93ffb20
youmerite <7wzkrqvvbw@privaterelay.appleid.com>
2026-01-17 22:52:25 +0100

    rev

--- api/stest.js
+++
@@ -1,9 +0,0 @@
-{
-  "name": "Test",
-  "phone": "123",
-  "city": "TestŞehir",
-  "delivery_address": "Adres",
-  "delivery_type": "Normal",
-  "cart_items": ["Ürün1", "Ürün2"],
-  "total_price": 100
-}
